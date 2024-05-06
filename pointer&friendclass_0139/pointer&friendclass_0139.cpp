518811831d8af331882e3974108240b6ce59b9c8
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-06 10:54:48 +0700

    membuat class siswa

--- pointer&friendclass_0139/pointer&friendclass_0139.cpp
+++ pointer&friendclass_0139/pointer&friendclass_0139.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class mahasiswa {
+public:
+    int nim;
+    void ShowNim() {
+        cout << "No induk = " << nim << endl;
+    }
+};
+
+int main() {
+    mahasiswa mhs{ 1 };
+    mhs.ShowNim();
+
+    mahasiswa& refmhs = mhs;
+    refmhs.nim = 2;
+    mhs.ShowNim();
+
+    mahasiswa* pMhs = &mhs;
+    pMhs->nim = 1;
+    mhs.ShowNim();
+    return 0;
 }
 
---
+++ pointerandfriendclass_139/pointerandfriendclass_139.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    void shownim()
+    {
+        cout << "no induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa* mhs = new mahasiswa{ 1 };
+    mhs->nim = 2;
+    mhs->shownim();
+    delete mhs;
+    return 0;
+}
+
+
