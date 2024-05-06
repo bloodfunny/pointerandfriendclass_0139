02cfd910aeb55312789f9fdb0f2227c26df1b371
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-06 11:41:36 +0700

    membuat int main

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -39,3 +39,13 @@ void  siswa::setId(int pId)
 {
     id = pId;
 }
+
+int main()
+{
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayall(joko);
+    return 0
+}
